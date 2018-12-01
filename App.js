3820571008a9d32f4c60733c63303babb6e59db3
anh khoa nguyen le <ceo.goverment@gmail.com>
2018-12-02 01:11:22 +0700

    commit new1

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <View style={styles.header}>
-          <Text>Color picker</Text>
+          <Text>Color dasdqwe</Text>
         </View>
       </View>
     );
