bfaf244655d434627ca121f8f1718c4f2eecdd12
anh khoa nguyen le <ceo.goverment@gmail.com>
2018-12-02 01:09:22 +0700

    commit change

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text>Color picker</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    alignItems: 'center',
+    height: 70,
+    backgroundColor: '#ECEFF1',
+    justifyContent: 'center',
+    shadowColor: 'gray',
+    shadowOffset: {
+      width: 0,
+      height: 5
+    },
+    shadowOpacity: 0.3,
+    paddingTop: 20,
+    elevation: 2
+  }
+});
