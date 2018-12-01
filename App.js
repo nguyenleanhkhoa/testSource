f0291151bbc93be837ac8605e62f564a617c77ef
anh khoa nguyen le <ceo.goverment@gmail.com>
2018-12-02 01:22:54 +0700

    commit qqweqrqas

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <View style={styles.header}>
-          <Text>nguyen leasda anh khoa (adasdqwd)</Text>
+          <Text>nguyen r1gsdsvleasda anh khoa (adasdqwd)</Text>
           <Text>dangkhoa</Text>
         </View>
       </View>
